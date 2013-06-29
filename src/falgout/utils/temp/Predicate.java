6c99836236538f03c7cc6bfe55e0b439f235b511
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2013-06-29 13:33:21 -0400

    Implemented JLS 15.12.2.1
    http://docs.oracle.com/javase/specs/jls/se7/html/jls-15.html#jls-15.12.2.1

---
+++ src/falgout/utils/temp/Predicate.java
@@ -0,0 +1,14 @@
+package falgout.utils.temp;
+
+/**
+ * Placeholder for Java 8's
+ * <a href=
+ * "http://download.java.net/jdk8/docs/api/java/util/function/Predicate.html">
+ * {@code Predicate}</a>
+ * 
+ * @author jeffrey
+ * @param <T> the type of argument to test
+ */
+public interface Predicate<T> {
+	public boolean test(T t);
+}
---
+++ test/falgout/utils/reflection/PotentiallyApplicableTest.java
@@ -0,0 +1,50 @@
+package falgout.utils.reflection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+public class PotentiallyApplicableTest {
+	public static void foo(Object one, Object two, Object... objects) {
+	}
+	
+	public static void foo(Object one, Object two, Object three) {
+	}
+	
+	private static final Method varargMethod;
+	private static final Method regularMethod;
+	static {
+		try {
+			varargMethod = PotentiallyApplicableTest.class.getMethod("foo", Object.class, Object.class, Object[].class);
+			regularMethod = PotentiallyApplicableTest.class.getMethod("foo", Object.class, Object.class, Object.class);
+		} catch (NoSuchMethodException e) {
+			throw new Error(e);
+		}
+	}
+	
+	@Test
+	public void NameMustBeCorrect() {
+		PotentiallyApplicable a = new PotentiallyApplicable("bogus", 3);
+		assertFalse(a.test(varargMethod));
+		assertFalse(a.test(regularMethod));
+	}
+	
+	@Test
+	public void RegularArityMustBeExact() {
+		for (int x = 0; x < 10; x++) {
+			PotentiallyApplicable a = new PotentiallyApplicable("foo", x);
+			assertEquals(x == 3, a.test(regularMethod));
+		}
+	}
+	
+	@Test
+	public void VariableArityMustBeGreaterThanNMinusOne() {
+		for (int x = 0; x < 10; x++) {
+			PotentiallyApplicable a = new PotentiallyApplicable("foo", x);
+			assertEquals(x >= (3 - 1), a.test(varargMethod));
+		}
+	}
+}
